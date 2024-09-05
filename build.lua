2389ec7be3083443196b7e74a5575b9f38dab659
alemi <me@alemi.dev>
2024-09-05 05:45:46 +0200

    fix: print when downloading
    should not be a silent action

--- build.lua
+++ build.lua
@@ -21,5 +21,6 @@ local plugin_dir = vim.fn.fnamemodify(debug.getinfo(1, "S").source:sub(2), ":p:h
 -- end
 local native_path = plugin_dir .. "/lua/codemp/native.so" -- TODO get extension based on platform
 local download_url = "https://codemp.dev/releases/lua/codemp_native-linux.so" -- TODO get url based on platform
+print("downloading codemp native lua extension...")
 
 vim.system({"curl", "-s", "-o", native_path, download_url }):wait() -- TODO can we run this asynchronously?
