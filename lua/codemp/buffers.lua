d0d1fb73aeb35361f74c32b72d8daca541666762
alemi <me@alemi.dev>
2024-09-27 23:35:26 +0200

    fix: invoke attach callback on main thread

--- lua/codemp/buffers.lua
+++ lua/codemp/buffers.lua
@@ -57,137 +57,140 @@ local function attach(name, opts)
 
 	vim.api.nvim_set_option_value('fileformat', 'unix', { buf = buffer })
 	vim.api.nvim_buf_set_name(buffer, name)
+
 	CODEMP.workspace:attach(name):and_then(function (controller)
-		-- TODO disgusting! but poll blocks forever on empty buffers...
-		if not opts.nowait then
-			local promise = controller:poll()
-			for i=1, 20, 1 do
-				if promise.ready then break end
-				vim.uv.sleep(100)
+		vim.schedule(function()
+			-- TODO disgusting! but poll blocks forever on empty buffers...
+			if not opts.nowait then
+				local promise = controller:poll()
+				for i=1, 20, 1 do
+					if promise.ready then break end
+					vim.uv.sleep(100)
+				end
 			end
-		end
 
-		if opts.window ~= nil then
-			vim.api.nvim_win_set_buf(opts.window, buffer)
-			vim.api.nvim_set_current_win(opts.window)
-		end
+			if opts.window ~= nil then
+				vim.api.nvim_win_set_buf(opts.window, buffer)
+				vim.api.nvim_set_current_win(opts.window)
+			end
 
-		-- TODO map name to uuid
+			-- TODO map name to uuid
 
-		id_buffer_map[buffer] = name
-		buffer_id_map[name] = buffer
-		ticks[buffer] = 0
+			id_buffer_map[buffer] = name
+			buffer_id_map[name] = buffer
+			ticks[buffer] = 0
 
-		-- hook serverbound callbacks
-		-- TODO breaks when deleting whole lines at buffer end
-		vim.api.nvim_buf_attach(buffer, false, {
-			on_bytes = function(_, buf, tick, start_row, start_col, start_offset, old_end_row, old_end_col, old_end_byte_len, new_end_row, new_end_col, new_byte_len)
-				if tick <= ticks[buf] then return end
-				if id_buffer_map[buf] == nil then return true end -- unregister callback handler
-				if CODEMP.config.debug then print(string.format(
-					"start(row:%s, col:%s) offset:%s end(row:%s, col:%s new(row:%s, col:%s)) len(old:%s, new:%s)",
-					start_row, start_col, start_offset, old_end_row, old_end_col, new_end_row, new_end_col, old_end_byte_len, new_byte_len
-				)) end
-				local end_offset = start_offset + old_end_byte_len
-				local change_content = ""
-				local len = utils.buffer.len(buf)
-				if start_offset + new_byte_len + 1 > len then
-					-- i dont know why but we may go out of bounds when doing 'dd' at the end of buffer??
-					local delta = (start_offset + new_byte_len + 1) - len
-					if CODEMP.config.debug then print("/!\\ bytes out of bounds by " .. delta .. ", adjusting") end
-					end_offset = end_offset - delta
-					start_offset = start_offset - delta
-				end
-				if new_byte_len > 0 then
-					local actual_end_col = new_end_col
-					if new_end_row == 0 then actual_end_col = new_end_col + start_col end
-					local actual_end_row = start_row + new_end_row
-					-- -- when bulk inserting at the end we go out of bounds, so we probably need to clamp?
-					-- --  issue: row=x+1 col=0 and row=x col=0 may be very far apart! we need to get last col of row x, ughh..
-					-- -- also, this doesn't work so it will stay commented out for a while longer
-					-- if new_end_row ~= old_end_row and new_end_col == 0 then
-					-- 	-- we may be dealing with the last line of the buffer, get_text could error because out-of-bounds
-					-- 	local row_count = vim.api.nvim_buf_line_count(buf)
-					-- 	if actual_end_row + 1 > row_count then
-					-- 		local delta = (actual_end_row + 1) - row_count
-					-- 		if CODEMP.config.debug then print("/!\\ row out of bounds by " .. delta .. ", adjusting") end
-					-- 		actual_end_row = actual_end_row - delta
-					-- 		actual_end_col = len - vim.api.nvim_buf_get_offset(buf, row_count)
-					-- 	end
-					-- end
-					local lines = vim.api.nvim_buf_get_text(buf, start_row, start_col, actual_end_row, actual_end_col, {})
-					change_content = table.concat(lines, '\n')
-				end
-				if CODEMP.config.debug then
-					print(string.format("sending: %s..%s '%s'", start_offset, start_offset + old_end_byte_len, change_content))
-				end
-				controller:send({
-					start = start_offset, finish = end_offset, content = change_content
-				}):await()
-			end,
-		})
+			-- hook serverbound callbacks
+			-- TODO breaks when deleting whole lines at buffer end
+			vim.api.nvim_buf_attach(buffer, false, {
+				on_bytes = function(_, buf, tick, start_row, start_col, start_offset, old_end_row, old_end_col, old_end_byte_len, new_end_row, new_end_col, new_byte_len)
+					if tick <= ticks[buf] then return end
+					if id_buffer_map[buf] == nil then return true end -- unregister callback handler
+					if CODEMP.config.debug then print(string.format(
+						"start(row:%s, col:%s) offset:%s end(row:%s, col:%s new(row:%s, col:%s)) len(old:%s, new:%s)",
+						start_row, start_col, start_offset, old_end_row, old_end_col, new_end_row, new_end_col, old_end_byte_len, new_byte_len
+					)) end
+					local end_offset = start_offset + old_end_byte_len
+					local change_content = ""
+					local len = utils.buffer.len(buf)
+					if start_offset + new_byte_len + 1 > len then
+						-- i dont know why but we may go out of bounds when doing 'dd' at the end of buffer??
+						local delta = (start_offset + new_byte_len + 1) - len
+						if CODEMP.config.debug then print("/!\\ bytes out of bounds by " .. delta .. ", adjusting") end
+						end_offset = end_offset - delta
+						start_offset = start_offset - delta
+					end
+					if new_byte_len > 0 then
+						local actual_end_col = new_end_col
+						if new_end_row == 0 then actual_end_col = new_end_col + start_col end
+						local actual_end_row = start_row + new_end_row
+						-- -- when bulk inserting at the end we go out of bounds, so we probably need to clamp?
+						-- --  issue: row=x+1 col=0 and row=x col=0 may be very far apart! we need to get last col of row x, ughh..
+						-- -- also, this doesn't work so it will stay commented out for a while longer
+						-- if new_end_row ~= old_end_row and new_end_col == 0 then
+						-- 	-- we may be dealing with the last line of the buffer, get_text could error because out-of-bounds
+						-- 	local row_count = vim.api.nvim_buf_line_count(buf)
+						-- 	if actual_end_row + 1 > row_count then
+						-- 		local delta = (actual_end_row + 1) - row_count
+						-- 		if CODEMP.config.debug then print("/!\\ row out of bounds by " .. delta .. ", adjusting") end
+						-- 		actual_end_row = actual_end_row - delta
+						-- 		actual_end_col = len - vim.api.nvim_buf_get_offset(buf, row_count)
+						-- 	end
+						-- end
+						local lines = vim.api.nvim_buf_get_text(buf, start_row, start_col, actual_end_row, actual_end_col, {})
+						change_content = table.concat(lines, '\n')
+					end
+					if CODEMP.config.debug then
+						print(string.format("sending: %s..%s '%s'", start_offset, start_offset + old_end_byte_len, change_content))
+					end
+					controller:send({
+						start = start_offset, finish = end_offset, content = change_content
+					}):await()
+				end,
+			})
 
-		local lock = false
-		local async = vim.loop.new_async(vim.schedule_wrap(function ()
-			if lock then return end
-			lock = true
-			while true do
-				local event = controller:try_recv():await()
-				if event == nil then break end
-				ticks[buffer] = vim.api.nvim_buf_get_changedtick(buffer)
-				CODEMP.ignore_following_action = true
-				if CODEMP.config.debug then
-					print(" ~~ applying change ~~ " .. event.start .. ".." .. event.finish .. "::[" .. event.content .. "]")
-				end
-				utils.buffer.set_content(buffer, event.content, event.start, event.finish)
-				if event.hash ~= nil then
-					if CODEMP.native.hash(utils.buffer.get_content(buffer)) ~= event.hash then
-						if CODEMP.config.auto_sync then
-							print(" /!\\ out of sync, resynching...")
-							utils.buffer.set_content(buffer, controller:content():await())
-						else
-							vim.ui.select(
-								{ "sync", "detach" },
-								{ prompt = "out of sync! force resync or detach?" },
-								function (choice)
-									if not choice then return end
-									if choice == "sync" then
-										utils.buffer.set_content(buffer, controller:content():await())
+			local lock = false
+			local async = vim.loop.new_async(vim.schedule_wrap(function ()
+				if lock then return end
+				lock = true
+				while true do
+					local event = controller:try_recv():await()
+					if event == nil then break end
+					ticks[buffer] = vim.api.nvim_buf_get_changedtick(buffer)
+					CODEMP.ignore_following_action = true
+					if CODEMP.config.debug then
+						print(" ~~ applying change ~~ " .. event.start .. ".." .. event.finish .. "::[" .. event.content .. "]")
+					end
+					utils.buffer.set_content(buffer, event.content, event.start, event.finish)
+					if event.hash ~= nil then
+						if CODEMP.native.hash(utils.buffer.get_content(buffer)) ~= event.hash then
+							if CODEMP.config.auto_sync then
+								print(" /!\\ out of sync, resynching...")
+								utils.buffer.set_content(buffer, controller:content():await())
+							else
+								vim.ui.select(
+									{ "sync", "detach" },
+									{ prompt = "out of sync! force resync or detach?" },
+									function (choice)
+										if not choice then return end
+										if choice == "sync" then
+											utils.buffer.set_content(buffer, controller:content():await())
+										end
+										if choice == "detach" then
+											detach(name)
+										end
 									end
-									if choice == "detach" then
-										detach(name)
-									end
-								end
-							)
+								)
+							end
+							return
 						end
-						return
 					end
 				end
+				lock = false
+			end))
+
+			local remote_content = controller:content():await()
+			if opts.content ~= nil then
+				-- TODO this may happen too soon!!
+				local _ = controller:send({
+					start = 0, finish = #remote_content, content = opts.content
+				}) -- no need to await
+			else
+				local current_content = utils.buffer.get_content(buffer)
+				if current_content ~= remote_content then
+					ticks[buffer] = vim.api.nvim_buf_get_changedtick(buffer)
+					utils.buffer.set_content(buffer, remote_content)
+				end
 			end
-			lock = false
-		end))
 
-		local remote_content = controller:content():await()
-		if opts.content ~= nil then
-			-- TODO this may happen too soon!!
-			local _ = controller:send({
-				start = 0, finish = #remote_content, content = opts.content
-			}) -- no need to await
-		else
-			local current_content = utils.buffer.get_content(buffer)
-			if current_content ~= remote_content then
-				ticks[buffer] = vim.api.nvim_buf_get_changedtick(buffer)
-				utils.buffer.set_content(buffer, remote_content)
-			end
-		end
+			controller:callback(function (_) async:send() end)
+			vim.defer_fn(function() async:send() end, 500) -- force a try_recv after 500ms
 
-		controller:callback(function (_) async:send() end)
-		vim.defer_fn(function() async:send() end, 500) -- force a try_recv after 500ms
-
-		local filetype = vim.filetype.match({ buf = buffer })
-		vim.api.nvim_set_option_value("filetype", filetype, { buf = buffer })
-		print(" ++ attached to buffer " .. name)
-		require('codemp.window').update()
+			local filetype = vim.filetype.match({ buf = buffer })
+			vim.api.nvim_set_option_value("filetype", filetype, { buf = buffer })
+			print(" ++ attached to buffer " .. name)
+			require('codemp.window').update()
+		end)
 	end)
 end
 
