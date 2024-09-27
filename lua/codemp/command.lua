eb240ec6fc54adb694d63213848750bd65fbf52b
alemi <me@alemi.dev>
2024-09-27 23:46:35 +0200

    fix: skip existing check for share commands

--- lua/codemp/command.lua
+++ lua/codemp/command.lua
@@ -104,7 +104,7 @@ local joined_actions = {
 			local buf = vim.api.nvim_get_current_buf()
 			if not bang then buffers.create(path) end
 			local content = utils.buffer.get_content(buf)
-			buffers.attach(path, { buffer = buf, content = content })
+			buffers.attach(path, { buffer = buf, content = content, skip_exists_check = true })
 			require('codemp.window').update() -- TODO would be nice to do automatically inside
 		else
 			print(" !! empty path or open a file")
